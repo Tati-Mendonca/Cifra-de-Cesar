9c044bdaca68eadf2c2b3b58e7e784284b04a8dc
Tatiane Mendonça <tatimendonz@gmail.com>
2022-01-31 14:01:59 -0300

    codigo refatorado e cifrando palavras completas

--- src/cipher.js
+++ src/cipher.js
@@ -1,32 +1,21 @@
-const cipher = {
 
+const cipher = { 
+
+  encode: function encode(offset,string){
+
+  
+  let newString = ""
+            for (let i = 0; i < string.length; i++) {
+                let codigoAsc = string.charCodeAt(i);
+                let cod1aLetra = 65;
+    
+                let valorEncode = ((codigoAsc - cod1aLetra + offset)% 26) + cod1aLetra;
+                let textCipher = String.fromCharCode(valorEncode);
+                newString += textCipher
+      
+            }
+            console.log(newString);
+            return newString
+    }
   }
-
-
-let entrada = document.getElementById("entrada")
-let saida = document.getElementById("saida")
-let number = document.getElementById("number");
-const alfabeto = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-//console.log (alfabeto);
-
-for (let i = 0; i < alfabeto.length; i++) { //loop nas letras do Alfabeto
-    console.log(i,alfabeto)
-}
-
-
-entrada.onkeyup = function(){ // textarea entrada qnd alguem digita o valor é enviado a textarea saida
-  let valor = entrada.value.toUpperCase(); 
-  let codigoDaLetraAsc = valor.charCodeAt() - 65; //codigo funcionando Alfabeto inicia em 0
-  let deslocamento = parseInt(codigoDaLetraAsc) + parseInt(number.value); // código somando corretamente!
-  let retornarCodAsc = deslocamento % 26 + 65; // retorno do alfabeto
-  let code = String.fromCharCode(retornarCodAsc)
-
-  saida.value = code; //caixa de saida
-}
-
-
-  export default cipher;
-
-
-
+export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import cipher from './cipher.js';
-
-console.log(cipher);
+import cipher from './cipher.js';  
 
 
-
-
-
+let textCipher = document.getElementById("string");
+textCipher.addEventListener("keyup", encode);
+        
+function encode() {
+    let string = document.getElementById("string").value.toUpperCase();
+    let offset = Number(document.getElementById("offset").value);
+    let encodeCipher = cipher.encode(offset, string)
+    document.getElementById("saida").value = encodeCipher    
+}
