4002750c5c540c291bc7c65012786003426ac384
Tatiane Mendonça <tatimendonz@gmail.com>
2022-01-31 17:29:46 -0300

    codigo funcionando para cifrar e decifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,21 +1,40 @@
 
-const cipher = { 
+ const cipher = { 
 
-  encode: function encode(offset,string){
+//   encode: function encode(offset,string){
 
   
-  let newString = ""
-            for (let i = 0; i < string.length; i++) {
-                let codigoAsc = string.charCodeAt(i);
-                let cod1aLetra = 65;
+//   let newString = ""
+//             for (let i = 0; i < string.length; i++) {
+//                 let codigoAsc = string.charCodeAt(i);
+//                 let cod1aLetra = 65;
     
-                let valorEncode = ((codigoAsc - cod1aLetra + offset)% 26) + cod1aLetra;
-                let textCipher = String.fromCharCode(valorEncode);
-                newString += textCipher
+//                 let valorEncode = ((codigoAsc - cod1aLetra + offset)% 26) + cod1aLetra;
+//                 let textCipher = String.fromCharCode(valorEncode);
+//                 newString += textCipher
       
-            }
-            console.log(newString);
-            return newString
-    }
+//             }
+//             console.log(newString);
+//             return newString
+//     }
+//   }
+
+  decode: function decode(offset,string){
+
+  
+    let newString = ""
+              for (let i = 0; i < string.length; i++) {
+                  let codigoAsc = string.charCodeAt(i);
+                  let codUltimaLetra = 90;
+      
+                  let valorDecode = ((codigoAsc - codUltimaLetra - offset)% 26) + codUltimaLetra;
+                  let textDecipher = String.fromCharCode(valorDecode);
+                  newString += textDecipher
+        
+              }
+              console.log(newString);
+              return newString
+      
   }
+}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import cipher from './cipher.js';  
 
 
-let textCipher = document.getElementById("string");
-textCipher.addEventListener("keyup", encode);
+// let textCipher = document.getElementById("string");
+// textCipher.addEventListener("keyup", encode);
         
-function encode() {
+// function encode() {
+//     let string = document.getElementById("string").value.toUpperCase();
+//     let offset = Number(document.getElementById("offset").value);
+//     let encodeCipher = cipher.encode(offset, string)
+//     document.getElementById("saida").value = encodeCipher    
+// }
+
+
+
+// if {       //Se usuário clicar em botão decifrar executar função abaixo
+//     let button = document.getElementById("button");
+//     button.addEventListener("click", decode)
+// }
+
+
+
+let textCipher = document.getElementById("string");
+textCipher.addEventListener("keyup", decode);
+        
+function decode() {
     let string = document.getElementById("string").value.toUpperCase();
     let offset = Number(document.getElementById("offset").value);
-    let encodeCipher = cipher.encode(offset, string)
-    document.getElementById("saida").value = encodeCipher    
+    let decodeCipher = cipher.decode(offset, string)
+    document.getElementById("saida").value = decodeCipher    
 }
+
+
+
