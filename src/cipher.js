50aa495c07b50a2f07efecde404c609085769823
Tatiane Mendonça <tatimendonz@gmail.com>
2022-02-01 17:32:49 -0300

    A lógica do projeto esta pronta, falta apenas alguns ajustes

--- src/cipher.js
+++ src/cipher.js
@@ -1,40 +1,36 @@
 
- const cipher = { 
+const cipher = { 
 
-//   encode: function encode(offset,string){
+  encode(offset,string){
 
-  
-//   let newString = ""
-//             for (let i = 0; i < string.length; i++) {
-//                 let codigoAsc = string.charCodeAt(i);
-//                 let cod1aLetra = 65;
-    
-//                 let valorEncode = ((codigoAsc - cod1aLetra + offset)% 26) + cod1aLetra;
-//                 let textCipher = String.fromCharCode(valorEncode);
-//                 newString += textCipher
-      
-//             }
-//             console.log(newString);
-//             return newString
-//     }
-//   }
-
-  decode: function decode(offset,string){
-
-  
     let newString = ""
+            for (let i = 0; i < string.length; i++) {
+                let codigoAsc = string.charCodeAt(i);
+                let cod1aLetra = 65;
+                let valorEncode = ((codigoAsc - cod1aLetra + offset)% 26) + cod1aLetra;
+                let textCipher = String.fromCharCode(valorEncode);
+                newString += textCipher
+      
+            }
+            console.log(newString);
+            return newString
+ },
+
+decode(offset,string){
+
+
+    let decipherString = "";
               for (let i = 0; i < string.length; i++) {
-                  let codigoAsc = string.charCodeAt(i);
-                  let codUltimaLetra = 90;
-      
-                  let valorDecode = ((codigoAsc - codUltimaLetra - offset)% 26) + codUltimaLetra;
-                  let textDecipher = String.fromCharCode(valorDecode);
-                  newString += textDecipher
-        
-              }
-              console.log(newString);
-              return newString
-      
+                let codigoAsc1 = string.charCodeAt(i);
+                let codUltimaLetra = 90;
+
+                let valorDecode = ((codigoAsc1 - codUltimaLetra - offset) % 26) + codUltimaLetra;
+                let textDecipher = String.fromCharCode(valorDecode);
+                decipherString += textDecipher;
+
+    }
+    console.log(decipherString);
+    return decipherString;
   }
-}
+} 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,26 @@
 import cipher from './cipher.js';  
 
-
-// let textCipher = document.getElementById("string");
-// textCipher.addEventListener("keyup", encode);
         
-// function encode() {
-//     let string = document.getElementById("string").value.toUpperCase();
-//     let offset = Number(document.getElementById("offset").value);
-//     let encodeCipher = cipher.encode(offset, string)
-//     document.getElementById("saida").value = encodeCipher    
-// }
-
-
-
-// if {       //Se usuário clicar em botão decifrar executar função abaixo
-//     let button = document.getElementById("button");
-//     button.addEventListener("click", decode)
-// }
-
-
-
-let textCipher = document.getElementById("string");
-textCipher.addEventListener("keyup", decode);
-        
-function decode() {
+function encode() {
     let string = document.getElementById("string").value.toUpperCase();
     let offset = Number(document.getElementById("offset").value);
-    let decodeCipher = cipher.decode(offset, string)
-    document.getElementById("saida").value = decodeCipher    
+    let encodeCipher = cipher.encode(offset, string)
+    document.getElementById("saida").value = encodeCipher    
+    
 }
 
 
+ //Se usuário clicar em botão decifrar executar função abaixo
 
+    function decode (e){
+        e.preventDefault() 
+        
+        let string1 = document.getElementById("string").value.toUpperCase();
+        let offset1 = Number(document.getElementById("offset").value);
+        let decodeCipher = cipher.decode(offset1, string1)
+        document.getElementById("saida").value = decodeCipher    
+}
+
+document.getElementById("buttonDecode").addEventListener("click", decode)
+let textCipher = document.getElementById("string");
+textCipher.addEventListener("keyup", encode);
