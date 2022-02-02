3f1c038f63426d310e257545a4e0af758d3b4f23
Tatiane Mendonça <tatimendonz@gmail.com>
2022-02-02 17:48:07 -0300

    Updates

---
+++ cipher.js
@@ -0,0 +1,36 @@
+
+const cipher = { 
+  encode(offset,string){
+    let newString = ""
+            for (let i = 0; i < string.length; i++) {
+                let codigoAsc = string.charCodeAt(i);
+                let cod1aLetra = 65;
+                let valorEncode = ((codigoAsc - cod1aLetra + offset)% 26) + cod1aLetra;
+                let textCipher = String.fromCharCode(valorEncode);
+                newString += textCipher
+      
+            }
+            console.log(newString);
+            return newString
+ },
+
+decode(offset,string){
+
+    let decipherString = "";
+              for (let i = 0; i < string.length; i++) {
+                let codigoAsc1 = string.charCodeAt(i);
+                let codUltimaLetra = 90;
+
+                let valorDecode = ((codigoAsc1 - codUltimaLetra - offset) % 26) + codUltimaLetra;
+                let textDecipher = String.fromCharCode(valorDecode);
+                decipherString += textDecipher;
+
+    }
+    console.log(decipherString);
+    return decipherString;
+  
+  }
+} 
+
+
+export default cipher;
---
+++ index.js
@@ -0,0 +1,44 @@
+
+import cipher from './cipher.js';  
+
+        
+function encode() {
+    let string = document.getElementById("string").value.toUpperCase();
+    let offset = Number(document.getElementById("offset").value);
+    let encodeCipher = cipher.encode(offset, string)
+    document.getElementById("saida").value = encodeCipher    
+    
+}
+
+ //Se usuário clicar em botão decifrar executar função abaixo
+
+    function decode (e){
+        e.preventDefault() 
+        
+        let string1 = document.getElementById("string").value.toUpperCase();
+        let offset1 = Number(document.getElementById("offset").value);
+        let decodeCipher = cipher.decode(offset1, string1)
+        document.getElementById("saida").value = decodeCipher    
+}
+
+document.getElementById("buttonDecode").addEventListener("click", decode)
+let textCipher = document.getElementById("string");
+textCipher.addEventListener("keyup", encode);
+
+//Função div oculta
+
+
+document.getElementById("saiba-mais").addEventListener("click", demoDisplay);
+
+
+    function demoDisplay(e) {
+        e.preventDefault()
+    
+        let info = document.getElementById("secret");
+        if(info.style.display === "block"){
+            info.style.display = "none";
+        } else {
+            info.style.display = "block";
+        }
+   
+}
