ac2274c65f26e765772476b9d443126a62a43b42
Tatiane Mendonça <tatimendonz@gmail.com>
2022-02-04 15:50:14 -0300

    Updates

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
 
 import cipher from './cipher.js';  
 
+document.getElementById("string").addEventListener("keyup", encode);
         
 function encode() {
     let string = document.getElementById("string").value.toUpperCase();
     let offset = Number(document.getElementById("offset").value);
     let encodeCipher = cipher.encode(offset, string)
-    document.getElementById("saida").value = encodeCipher    
+    document.getElementById("exit").value = encodeCipher    
     
 }
 
- //Se usuário clicar em botão decifrar executar função abaixo
+document.getElementById("buttonDecode").addEventListener("click", decode);
 
-    function decode (e){
-        e.preventDefault() 
+function decode (e){
+    e.preventDefault() 
         
-        let string1 = document.getElementById("string").value.toUpperCase();
-        let offset1 = Number(document.getElementById("offset").value);
-        let decodeCipher = cipher.decode(offset1, string1)
-        document.getElementById("saida").value = decodeCipher    
+    let string1 = document.getElementById("string").value.toUpperCase();
+    let offset1 = Number(document.getElementById("offset").value);
+    let decodeCipher = cipher.decode(offset1, string1)
+    document.getElementById("exit").value = decodeCipher  
+        
 }
 
-document.getElementById("buttonDecode").addEventListener("click", decode)
-let textCipher = document.getElementById("string");
-textCipher.addEventListener("keyup", encode);
 
 //Função div oculta
 
+document.getElementById("btn-div-secret").addEventListener("click", display);
 
-document.getElementById("saiba-mais").addEventListener("click", demoDisplay);
 
-
-    function demoDisplay(e) {
+    function display(e) {
         e.preventDefault()
     
         let info = document.getElementById("secret");
@@ -42,3 +40,7 @@ document.getElementById("saiba-mais").addEventListener("click", demoDisplay);
         }
    
 }
+
+
+
+
