770d85fb4919d10702172eefa8acfeb99033b169
Tatiane Mendonça <tatimendonz@gmail.com>
2022-02-08 02:30:04 -0300

    inclui mensagens de erro, projeto finalizado

--- src/cipher.js
+++ src/cipher.js
@@ -1,56 +1,73 @@
 const cipher = {
   encode(offset, string) {
     let newString = ""
+
+    if (typeof string !== "string") {
+      throw TypeError('A mensagem precisa ser um texto')
+    }
+
+
     for (let i = 0; i < string.length; i++) {
       let codAsc = string.charCodeAt(i);
 
-      if (codAsc <= 90){
+      if (codAsc >= 65 && codAsc <= 90) {
         let firstLetter = 65;
         let valueEncode = ((codAsc - firstLetter + offset) % 26) + firstLetter;
         let cipherText = String.fromCharCode(valueEncode);
         newString += cipherText
-        
-        
-      } else{
-        (codAsc >= 122)
-          let firstLetter = 97;
-          let valueEncode = ((codAsc - firstLetter + offset) % 26) + firstLetter;
-          let cipherText = String.fromCharCode(valueEncode);
-          newString += cipherText
-        
-        }
+
+
+      } else {
+        (codAsc >= 97 && codAsc <= 122)
+        let firstLetter = 97;
+        let valueEncode = ((codAsc - firstLetter + offset) % 26) + firstLetter;
+        let cipherText = String.fromCharCode(valueEncode);
+        newString += cipherText
+
+      }
     }
-    
+
     return newString
-},
 
-decode(offset, string) {
 
-let decipherString = "";
-for (let i = 0; i < string.length; i++) {
-  let codAsc = string.charCodeAt(i);
+  },
 
-  if(codAsc <= 90){
-      let lastLetter = 90;
-      let valueDecode = ((codAsc - lastLetter - offset) % 26) + lastLetter;
-      let decipherText = String.fromCharCode(valueDecode);
-      decipherString += decipherText;
+  decode(offset, string) {
 
-    } else {
-      (codAsc >= 122)
-      let lastLetter = 122;
-      let valueDecode = ((codAsc - lastLetter - offset) % 26) + lastLetter;
-      let decipherText = String.fromCharCode(valueDecode);
-      decipherString += decipherText;
-    
+
+
+    if (typeof string !== "string") {
+      throw TypeError('A mensagem precisa ser um texto')
+    }
+
+    let decipherString = "";
+    for (let i = 0; i < string.length; i++) {
+      let codAsc = string.charCodeAt(i);
+
+
+
+      if (codAsc <= 90) {
+        let lastLetter = 90;
+        let valueDecode = ((codAsc - lastLetter - offset) % 26) + lastLetter;
+        let decipherText = String.fromCharCode(valueDecode);
+        decipherString += decipherText;
+
+      } else {
+        (codAsc >= 122)
+        let lastLetter = 122;
+        let valueDecode = ((codAsc - lastLetter - offset) % 26) + lastLetter;
+        let decipherText = String.fromCharCode(valueDecode);
+        decipherString += decipherText;
+
+      }
+
+    }
+
+    return decipherString;
+
   }
-
 }
 
-return decipherString;
-
-}
-}
 
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 
-import cipher from './cipher.js';  
+import cipher from './cipher.js';
 
 document.getElementById("string").addEventListener("keyup", encode);
-        
+
 function encode() {
     let string = document.getElementById("string").value;
     let offset = Number(document.getElementById("offset").value);
     let encodeCipher = cipher.encode(offset, string)
-    document.getElementById("exit").value = encodeCipher    
-    
+    document.getElementById("exit").value = encodeCipher
+
 }
 
 document.getElementById("buttonDecode").addEventListener("click", decode);
 
-function decode (e){
-    e.preventDefault() 
-        
+function decode(e) {
+    e.preventDefault()
+
     let string1 = document.getElementById("string").value;
     let offset1 = Number(document.getElementById("offset").value);
     let decodeCipher = cipher.decode(offset1, string1)
-    document.getElementById("exit").value = decodeCipher  
-        
+    document.getElementById("exit").value = decodeCipher
+
 }
 
 
@@ -29,16 +29,16 @@ function decode (e){
 document.getElementById("btn-div-secret").addEventListener("click", display);
 
 
-    function display(e) {
-        e.preventDefault()
-    
-        let info = document.getElementById("secret");
-        if(info.style.display === "block"){
-            info.style.display = "none";
-        } else {
-            info.style.display = "block";
-        }
-   
+function display(e) {
+    e.preventDefault()
+
+    let info = document.getElementById("secret");
+    if (info.style.display === "block") {
+        info.style.display = "none";
+    } else {
+        info.style.display = "block";
+    }
+
 }
 
 
