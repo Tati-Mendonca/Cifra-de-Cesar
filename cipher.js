78a427d4740ae562b247c766624d7384e60251bc
Tatiane Mendonça <tatimendonz@gmail.com>
2022-02-08 02:43:17 -0300

    Updates

--- cipher.js
+++ cipher.js
@@ -1,35 +1,73 @@
-
-const cipher = { 
-  encode(offset,string){
+const cipher = {
+  encode(offset, string) {
     let newString = ""
-            for (let i = 0; i < string.length; i++) {
-                let codAsc = string.charCodeAt(i);
-                let firstLetter = 65;
-                let valueEncode = ((codAsc - firstLetter + offset)% 26) + firstLetter;
-                let cipherText = String.fromCharCode(valueEncode);
-                newString += cipherText
-      
-            }
-          
-            return newString
- },
 
-decode(offset,string){
+    if (typeof string !== "string") {
+      throw TypeError('A mensagem precisa ser um texto')
+    }
+
+
+    for (let i = 0; i < string.length; i++) {
+      let codAsc = string.charCodeAt(i);
+
+      if (codAsc >= 65 && codAsc <= 90) {
+        let firstLetter = 65;
+        let valueEncode = ((codAsc - firstLetter + offset) % 26) + firstLetter;
+        let cipherText = String.fromCharCode(valueEncode);
+        newString += cipherText
+
+
+      } else {
+        (codAsc >= 97 && codAsc <= 122)
+        let firstLetter = 97;
+        let valueEncode = ((codAsc - firstLetter + offset) % 26) + firstLetter;
+        let cipherText = String.fromCharCode(valueEncode);
+        newString += cipherText
+
+      }
+    }
+
+    return newString
+
+
+  },
+
+  decode(offset, string) {
+
+
+
+    if (typeof string !== "string") {
+      throw TypeError('A mensagem precisa ser um texto')
+    }
 
     let decipherString = "";
-              for (let i = 0; i < string.length; i++) {
-                let codAsc = string.charCodeAt(i);
-                let lastLetter = 90;
-                let valueDecode = ((codAsc - lastLetter - offset)% 26) + lastLetter;
-                let decipherText = String.fromCharCode(valueDecode);
-                decipherString += decipherText;
+    for (let i = 0; i < string.length; i++) {
+      let codAsc = string.charCodeAt(i);
+
+
+
+      if (codAsc <= 90) {
+        let lastLetter = 90;
+        let valueDecode = ((codAsc - lastLetter - offset) % 26) + lastLetter;
+        let decipherText = String.fromCharCode(valueDecode);
+        decipherString += decipherText;
+
+      } else {
+        (codAsc >= 122)
+        let lastLetter = 122;
+        let valueDecode = ((codAsc - lastLetter - offset) % 26) + lastLetter;
+        let decipherText = String.fromCharCode(valueDecode);
+        decipherString += decipherText;
+
+      }
 
     }
- 
+
     return decipherString;
-  
+
   }
-} 
+}
+
 
 
 export default cipher;
