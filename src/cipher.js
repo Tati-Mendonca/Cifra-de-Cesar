5b69ac583cd5b4ab7e370664193fd65db5ca9e25
Tatiane Mendonça <97405991+Tati-Mendonca@users.noreply.github.com>
2022-06-05 01:53:37 -0300

    Refatoração

--- src/cipher.js
+++ src/cipher.js
@@ -1,40 +1,35 @@
 const cipher = {
-  encode(offset, string) {
+  encode: function (offset, string) {
     let newString = ""
 
     if (typeof string !== "string") {
       throw TypeError('A mensagem precisa ser um texto')
     }
 
-
     for (let i = 0; i < string.length; i++) {
       let codAsc = string.charCodeAt(i);
 
       if (codAsc >= 65 && codAsc <= 90) {
-        let firstLetter = 65;
-        let valueEncode = ((codAsc - firstLetter + offset) % 26) + firstLetter;
+
+        let lastLetter = 65;
+        let valueEncode = ((codAsc - lastLetter + offset) % 26) + lastLetter;
         let cipherText = String.fromCharCode(valueEncode);
         newString += cipherText
 
+      } else if (codAsc >= 97 && codAsc <= 122) {
+        let lastLetter = 97;
+        let valueEncode = ((codAsc - lastLetter + offset) % 26) + lastLetter;
+        let cipherText = String.fromCharCode(valueEncode);
+        newString += cipherText
 
       } else {
-        (codAsc >= 97 && codAsc <= 122)
-        let firstLetter = 97;
-        let valueEncode = ((codAsc - firstLetter + offset) % 26) + firstLetter;
-        let cipherText = String.fromCharCode(valueEncode);
-        newString += cipherText
-
+        newString += string.charAt(i);
       }
     }
-
     return newString
-
-
   },
 
   decode(offset, string) {
-
-
 
     if (typeof string !== "string") {
       throw TypeError('A mensagem precisa ser um texto')
@@ -43,8 +38,6 @@ const cipher = {
     let decipherString = "";
     for (let i = 0; i < string.length; i++) {
       let codAsc = string.charCodeAt(i);
-
-
 
       if (codAsc <= 90) {
         let lastLetter = 90;
@@ -58,16 +51,10 @@ const cipher = {
         let valueDecode = ((codAsc - lastLetter - offset) % 26) + lastLetter;
         let decipherText = String.fromCharCode(valueDecode);
         decipherString += decipherText;
-
       }
-
     }
-
     return decipherString;
-
   }
 }
-
-
 
 export default cipher;
